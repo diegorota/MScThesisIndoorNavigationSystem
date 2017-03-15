2f854306cd5ef72d49108c7c26b6f38e0f6bffe9
Montalto Simone <simonemontalto@macbook-pro-di-simone-montalto.local>
2017-03-15 10:13:17 +0100

    Initial Commit

---
+++ Hospital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hospital
+//
+//  Created by Simone Montalto on 15/03/17.
+//  Copyright © 2017 MontaltoRota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
